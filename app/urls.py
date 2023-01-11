55b2e3b4ce325de976f17de44aefa954a900b8c0
inesira <inesiradodi@gmail.com>
2023-01-11 12:31:47 +0200

    some modifications in urls.py

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,10 @@ from . import views
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', views.home.index, name='home'),
+    
+    path('category/', views.categories.index , name='category'),
+    path('category/create', views.categories.create, name='categorie_create'),
+    path('category/store', views.categories.store, name='categorie_store'),
+    path('category/edit/<int:id>', views.categories.edit, name='categorie_edit'),
+    path('category/delete/<int:id>', views.categories.delete, name='categorie_delete'),
 ]
