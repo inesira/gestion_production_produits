04d1a2e8726f41d7cb906e9a9c9d88e529198812
inesira <inesiradodi@gmail.com>
2023-01-18 13:09:03 +0200

    some modifications in urls.py

--- app/urls.py
+++ app/urls.py
@@ -18,4 +18,16 @@ urlpatterns = [
     path('stock/edit/<int:id>', views.stocks.edit, name='stock_edit'),
     path('stock/delete/<int:id>', views.stocks.delete, name='stock_delete'),
     path('stock/getStock_actuel', views.stocks.getStock_actuel, name='stock_getStock_actuel'),
+    
+    path('input/', views.inputs.index , name='input'),
+    path('input/create', views.inputs.create, name='input_create'),
+    path('input/store', views.inputs.store, name='input_store'),
+    path('input/edit/<int:id>', views.inputs.edit, name='input_edit'),
+    path('input/update/<int:id>', views.inputs.update, name='input_update'),
+    path('input/delete/<int:id>', views.inputs.delete, name='input_delete'),
+    path('input/getEntrees', views.inputs.getEntrees, name='input_getEntrees'),
+    path('input/getTotal', views.inputs.getTotal, name='input_getTotal'),
+    path('input/getTotale', views.inputs.getTotale, name='input_getTotale'),
+    path('input/getDate', views.inputs.getDate, name='input_getDate'),
+    
 ]
