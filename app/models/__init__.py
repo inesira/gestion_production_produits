e804d64f6014ae3986ba4bd4d11b44484f97de68
inesira <inesiradodi@gmail.com>
2023-01-18 16:32:46 +0200

    some modifications in in models/__init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,3 +4,4 @@ from .output import Output
 from .category import Category
 from .production import Production
 from .mains_d_oeuvres import Mains_d_oeuvres
+from .situation_stock import Situation_stock
