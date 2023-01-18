b7dcff5a1780811858336d17a1bcf90b37c2c38f
inesira <inesiradodi@gmail.com>
2023-01-18 16:32:20 +0200

    some modifications in forms/__init__.py

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -3,3 +3,4 @@ from .stock_form import StockForm
 from .input_form import InputForm
 from .output_form import OutputForm
 from .main_d_oeuvre_form import Main_d_oeuvreForm
+from .situation_stock_form import Situation_stockForm
