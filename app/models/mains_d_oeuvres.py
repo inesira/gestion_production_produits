74aec1ca5fe45451d31d200de1054000ff000174
inesira <inesiradodi@gmail.com>
2023-01-20 21:53:15 +0200

    some modifications in models

--- app/models/mains_d_oeuvres.py
+++ app/models/mains_d_oeuvres.py
@@ -1,12 +1,11 @@
 from django.db import models
-
 from app.models import Category
-
-
+from app.models import Production
 
 class Mains_d_oeuvres(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     nombre = models.FloatField()
     revenu = models.FloatField(null=True, blank=True)
     total = models.FloatField(null=True, blank=True)
+    production = models.ForeignKey(Production, on_delete=models.CASCADE, null=True, blank=True)
     date = models.DateField()
--- app/models/production.py
+++ app/models/production.py
@@ -1,19 +1,10 @@
 from django.db import models
-from app.models import Stock
-from app.models import Category
-
-
 
 class Production(models.Model):
     
-    produit_finis = models.CharField(max_length=100)
-    quantite_produite = models.FloatField()
-    unite_production = models.FloatField()
-    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    cout_production = models.FloatField()
-   
+    nom_prod = models.CharField(max_length=100)
+    unite_production = models.CharField(max_length=100)
     date = models.DateField()
     
     def __str__(self):
-        return self.produit_finis + "" + self.cout_production
+        return self.nom_prod
