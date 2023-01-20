e42982e812ebbd2704d4a8d70517d3d9ad6514ee
inesira <inesiradodi@gmail.com>
2023-01-20 23:04:16 +0200

    some modifications in models/product.py

--- app/models/product.py
+++ app/models/product.py
@@ -2,7 +2,11 @@ from django.db import models
 
 class Product(models.Model):
     
-    produit = models.CharField(max_length=100,null=True,blank=True)
+    produit = models.CharField(max_length=100)
     quantite = models.FloatField(null=True, blank=True)
     unite_production = models.CharField(max_length=100)
     date = models.DateField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.produit    
+  
