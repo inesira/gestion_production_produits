a4489418ebec6258a67388f6ac3e8eba44386ce3
inesira <inesiradodi@gmail.com>
2023-01-21 22:31:33 +0200

    some modifications in models/depense.py

--- app/models/depense.py
+++ app/models/depense.py
@@ -5,7 +5,9 @@ class Depense(models.Model):
     
     type_depense =models.CharField(max_length=100, null=True, blank=True)
     production= models.ForeignKey(Production, on_delete=models.CASCADE)
+    stocks = models.IntegerField(null=True, blank=True)
     cout_total_sorties = models.FloatField(null=True, blank=True)
+    categories = models.IntegerField(null=True, blank=True)
     cout_total_m = models.FloatField(null=True, blank=True)
     date = models.DateField(null=True, blank=True)
     
