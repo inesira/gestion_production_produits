f41e6380a8673905352eb6683356558c0d22f770
inesira <inesiradodi@gmail.com>
2023-01-23 15:06:25 +0200

    some modifications in models/__init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -7,3 +7,5 @@ from .mains_d_oeuvres import Mains_d_oeuvres
 from .situation_stock import Situation_stock
 from .depense import Depense
 from .product import Product
+from .cout_production import Cout_production
+
