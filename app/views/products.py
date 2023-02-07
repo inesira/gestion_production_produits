7f8bcd4d657b849d6379db1da0ebd5c183e8f152
inesira <inesiradodi@gmail.com>
2023-02-07 20:17:18 +0200

    some modifications in app/views/

--- app/views/products.py
+++ app/views/products.py
@@ -7,8 +7,8 @@ from django.contrib.auth.decorators import login_required,user_passes_test
 
 # Create your views here.
 @login_required( login_url="/login")
-@user_passes_test(lambda user: user.is_staff ,login_url="/error/gest")
-@user_passes_test(lambda user: not(user.is_superuser) ,login_url="/error/admin")
+@user_passes_test(lambda user: user.is_staff ,login_url="/error/resp")
+@user_passes_test(lambda user: user.is_superuser ,login_url="/error/gest")
 
 def index(request):
     assert isinstance(request, HttpRequest)
