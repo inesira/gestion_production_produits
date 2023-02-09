e25e56151f70865c40913384f9f64e579770c88e
inesira <inesiradodi@gmail.com>
2023-02-09 14:29:10 +0200

    some modifications in forms/

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,14 +2,11 @@ from .category_form import CategoryForm
 from .matierePremiere_form import MatierePremiereForm
 from .input_form import InputForm
 from .output_form import OutputForm
-from .main_d_oeuvre_form import Main_d_oeuvreForm
-from .situation_stock_form import Situation_stockForm
 from .production_form import ProductionForm
 from .depense_form import DepenseForm
 from .product_form import ProductForm
-from .cout_production_form import Cout_productionForm
 from .user_form import UserForm
 from .user_form_edit import UserFormEdit
 from .user_form_edit_pass import UserFormEditPass
 from .user_form_edit_info import UserFormEdit_inf
-from .stock_form import StockForm
+from .cout_form import CoutForm
