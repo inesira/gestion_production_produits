566cee917bfc51508292424359543499c34e5f14
inesira <inesiradodi@gmail.com>
2023-02-09 14:50:30 +0200

    some modifications in app/urls.py

--- app/urls.py
+++ app/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
    
     
     
-    path('category/', views.categories_resp.index , name='category'),
+    path('category/', views.categories.index , name='category'),
     path('category/create', views.categories.create, name='categorie_create'),
     path('category/store', views.categories.store, name='categorie_store'),
     path('category/edit/<int:id>', views.categories.edit, name='categorie_edit'),
@@ -37,35 +37,18 @@ urlpatterns = [
     path('output/delete/<int:id>', views.outputs.delete, name='output_delete'),
     path('output/getSorties', views.outputs.getSorties, name='output_getSorties'),
     path('output/getPrice', views.outputs.getPrice, name='output_getPrice'),
-    path('output/getUser', views.outputs.getUser, name='output_getUser'),
+    path('output/getUnite', views.outputs.getUnite, name='output_getUnite'),
+    path('output/getDate', views.outputs.getDate, name='output_getDate'),
     
-    path('mains_d_oeuvres/', views.main_d_oeuvres.index , name='mains_d_oeuvres'),
-    path('mains_d_oeuvres/create', views.main_d_oeuvres.create, name='mains_create'),
-    path('mains_d_oeuvres/store', views.main_d_oeuvres.store, name='mains_store'),
-    path('mains_d_oeuvres/edit/<int:id>', views.main_d_oeuvres.edit, name='mains_edit'),
-    path('mains_d_oeuvres/update/<int:id>', views.main_d_oeuvres.update, name='mains_update'),
-    path('mains_d_oeuvres/delete/<int:id>', views.main_d_oeuvres.delete, name='mains_delete'),
-    path('mains_d_oeuvres/getRevenu', views.main_d_oeuvres.getRevenu, name='mains_getRevenu'),
-  
-  
-  
-    path('situation_stock/', views.situation_stocks.index , name='situation_stock'),
-    path('situation_stock/create', views.situation_stocks.create, name='st_create'),
-    path('situation_stock/store', views.situation_stocks.store, name='st_store'),
-    path('situation_stock/edit/<int:id>', views.situation_stocks.edit, name='st_edit'),
-    path('situation_stock/update/<int:id>', views.situation_stocks.update, name='st_update'),
-    path('situation_stock/delete/<int:id>', views.situation_stocks.delete, name='st_delete'),
-    path('situation_stock/getStock_actuel', views.situation_stocks.getStock_actuel, name='st_getStock_actuel'),
-    path('situation_stock/getPrix_unitaire', views.situation_stocks.getPrix_unitaire, name='st_getPrix_unitaire'),
-    path('situation_stock/getPrixTotal', views.situation_stocks.getPrixTotal, name='st_getPrixTotal'),
-    path('situation_stock/getDat', views.situation_stocks.getDat, name='st_getDat'),
-    
-    
+
     path('production/', views.productions.index , name='production'),
     path('production/create', views.productions.create, name='pro_create'),
     path('production/store', views.productions.store, name='pro_store'),
     path('production/edit/<int:id>', views.productions.edit, name='pro_edit'),
+    path('production/update/<int:id>', views.productions.update, name='pro_update'),
     path('production/delete/<int:id>', views.productions.delete, name='pro_delete'),
+    path('production/getCout', views.productions.getCout, name='st_getCout'),
+    
     
     path('depense/', views.depenses.index , name='depense'),
     path('depense/create', views.depenses.create, name='dep_create'),
@@ -73,11 +56,7 @@ urlpatterns = [
     path('depense/edit/<int:id>', views.depenses.edit, name='dep_edit'),
     path('depense/update/<int:id>', views.depenses.update, name='dep_update'),
     path('depense/delete/<int:id>', views.depenses.delete, name='dep_delete'),
-    path('depense/getStocks', views.depenses.getStocks, name='dep_getStocks'),
-    path('depense/getTotal_sorties', views.depenses.getTotal_sorties, name='dep_getTotal_sorties'),
-    path('depense/getCategories', views.depenses.getCategories, name='dep_getCategories'),
-    path('depense/getTotal_m', views.depenses.getTotal_m, name='dep_getTotal_m'),
-    path('depense/getDate', views.depenses.getDate, name='dep_getDate'),
+    path('depense/getDate', views.depenses.getDate, name='st_getDate'),
     
     
     path('product/', views.products.index , name='product'),
@@ -87,16 +66,21 @@ urlpatterns = [
     path('product/delete/<int:id>', views.products.delete, name='prod_delete'),
     
     
-    path('cout_production/', views.cout_productions.index , name='cout_production'),
-    path('cout_production/create', views.cout_productions.create, name='cout_create'),
-    path('cout_production/store', views.cout_productions.store, name='cout_store'),
-    path('cout_production/edit/<int:id>', views.cout_productions.edit, name='cout_edit'),
-    path('cout_production/update/<int:id>', views.cout_productions.update, name='cout_update'),
-    path('cout_production/delete/<int:id>', views.cout_productions.delete, name='cout_delete'),
-    path('cout_production/getProduct', views.cout_productions.getProduct, name='cout_getProduct'),
-    path('cout_production/getDepense', views.cout_productions.getDepense, name='cout_getDepense'),
-    path('cout_production/getDate', views.cout_productions.getDate, name='cout_getDate'),
-    path('cout_production/getUnite', views.cout_productions.getUnite, name='cout_getUnite'),
+    path('cout/', views.couts.index , name='cout'),
+    path('cout/create', views.couts.create, name='cout_create'),
+    path('cout/store', views.couts.store, name='cout_store'),
+    path('cout/edit/<int:id>', views.couts.edit, name='cout_edit'),
+    path('cout/update/<int:id>', views.couts.update, name='cout_update'),
+    path('cout/delete/<int:id>', views.couts.delete, name='cout_delete'),
+    path('cout/getQuantite', views.couts.getQuantite, name='cout_getQuantite'),
+    path('cout/getSortie', views.couts.getSortie, name='cout_getSortie'),
+    path('cout/getTotal', views.couts.getTotal, name='cout_getTotal'),
+    path('cout/getDepense', views.couts.getDepense, name='cout_getDepense'),
+    path('cout/getCoutDepense', views.couts.getCoutDepense, name='cout_getCoutDepense'),
+    path('cout/getCoutSortie', views.couts.getCoutSortie, name='cout_getCoutSortie'),
+    # path('cout/getCoutProduction', views.couts.getCoutProduction, name='cout_getCoutProduction'),
+    
+    
     
     
      path('user/<int:id>', views.users.index, name='user'),
@@ -127,16 +111,13 @@ urlpatterns = [
     path('home_gest/', views.home_gest.index, name='home_gest'),
     
     # views pour l'admin
-    path('stock_admin/', views.stock_admin.index, name='stock_admin'),
     path('input_admin', views.input_admin.index, name='input_admin'),
     path('output_admin', views.output_admin.index, name='output_admin'),
-    path('situation_stock_admin', views.situation_stock_admin.index, name='situation_stock_admin'),
     path('category_admin', views.categories.index, name='category_admin'),
-    path('mains_d_oeuvres_admin', views.main_d_oeuvres_admin.index, name='mains_d_oeuvres_admin'),
     path('production_admin', views.production_admin.index, name='production_admin'),
     path('product_admin', views.product_admin.index, name='product_admin'),
     path('depense_admin', views.depense_admin.index, name='depense_admin'),
-    path('cout_production_admin/', views.cout_production_admin.index, name='cout_production_admin'),
+    #path('cout_production_admin/', views.cout_production_admin.index, name='cout_production_admin'),
     
     
     
